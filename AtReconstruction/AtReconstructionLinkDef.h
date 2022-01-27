9f783b9f4e856896137baf59e5c21b8ab3e91c33
Stephen Gillespie <gillespi@flagtail.nscl.msu.edu>
2022-01-27 10:52:48 -0500

    Committer: Stephen Gillespie <gillespi@flagtail.nscl.msu.edu>
     On branch develop
     Your branch is up to date with 'origin/develop'.
    
     Changes to be committed:
    /bin/bash: wq: command not found
    	deleted:    AtReconstruction/AtDecoder/AtEvent.h
    	deleted:    AtReconstruction/AtDecoder/AtHit.cxx
    	deleted:    AtReconstruction/AtDecoder/AtHit.h
    	deleted:    AtReconstruction/AtDecoder/AtHitCluster.cxx
    	deleted:    AtReconstruction/AtDecoder/AtHitCluster.h
    	deleted:    AtReconstruction/AtDecoder/AtPad.cxx
    	deleted:    AtReconstruction/AtDecoder/AtPad.h
    	deleted:    AtReconstruction/AtDecoder/AtPatternEvent.cxx
    	deleted:    AtReconstruction/AtDecoder/AtPatternEvent.h
    	deleted:    AtReconstruction/AtDecoder/AtProtoEvent.cxx
    	deleted:    AtReconstruction/AtDecoder/AtProtoEvent.h
    	deleted:    AtReconstruction/AtDecoder/AtProtoEventAna.cxx
    	deleted:    AtReconstruction/AtDecoder/AtProtoEventAna.h
    	deleted:    AtReconstruction/AtDecoder/AtProtoQuadrant.cxx
    	deleted:    AtReconstruction/AtDecoder/AtProtoQuadrant.h
    	deleted:    AtReconstruction/AtDecoder/AtRawEvent.cxx
    	deleted:    AtReconstruction/AtDecoder/AtRawEvent.h
    	deleted:    AtReconstruction/AtDecoder/AtTrack.cxx
    	deleted:    AtReconstruction/AtDecoder/AtTrack.h
    	deleted:    AtReconstruction/AtDecoder/AtTrackingEventAna.cxx
    	deleted:    AtReconstruction/AtDecoder/AtTrackingEventAna.h
    	modified:   AtReconstruction/AtReconstructionLinkDef.h
    	modified:   AtReconstruction/CMakeLists.txt
    	modified:   CMakeLists.txt
    
      Untracked files:
    	AtData/

--- AtReconstruction/AtReconstructionLinkDef.h
+++ AtReconstruction/AtReconstructionLinkDef.h
@@ -5,18 +5,6 @@
 #pragma link off all functions;
 
 #pragma link C++ class AtHDFParser + ;
-#pragma link C++ class AtPad + ;
-#pragma link C++ class AtRawEvent + ;
-#pragma link C++ class AtHit + ;
-#pragma link C++ class AtHitCluster + ;
-#pragma link C++ struct AtHit::MCSimPoint + ;
-#pragma link C++ class AtEvent + ;
-#pragma link C++ class AtProtoEvent + ;
-#pragma link C++ class AtProtoEventAna + ;
-#pragma link C++ class AtPatternEvent + ;
-#pragma link C++ class AtTrackingEventAna + ;
-#pragma link C++ class AtProtoQuadrant + ;
-#pragma link C++ class AtTrack + ;
 
 #pragma link C++ class AtPSA + ;
 #pragma link C++ class AtPSASimple + ;
@@ -54,7 +42,6 @@
 
 #pragma link C++ class AtRansacTask + ;
 
-#pragma link C++ struct AtTrack::FitPar + ;
 #pragma link C++ class AtHDFParserTask + ;
 
 //#pragma link C++ function AtHoughSpaceLine::CalcGenHoughSpace<AtEvent*>(GenHough event);

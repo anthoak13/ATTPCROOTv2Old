76a7b296be7dda0a8b90a2c317ad044d5f287ffe
Adam Anthony <anthonya@nscl.msu.edu>
2022-05-03 12:12:37 -0400

    Refactor new ransac to have a common base clase AtSampleConsensus
    The different estimators all derive from this abstract interface.

---
+++ AtReconstruction/AtPatternRecognition/AtSampleConsensus.cxx
@@ -0,0 +1,172 @@
+#include "AtSampleConsensus.h"
+
+#include "AtEvent.h" // for AtEvent
+#include "AtHit.h"   // for AtHit
+#include "AtModelFactory.h"
+#include "AtPatternEvent.h"
+#include "AtRandomSample.h"
+
+#include <Math/Point3D.h> // for PositionVector3D
+#include <TMath.h>        // for Pi
+#include <TRandom.h>      // for TRandom, gRandom
+
+#include <cmath>    // for cos, sin, pow, sqrt, fabs, exp, acos, atan
+#include <fstream>  // for std
+#include <iterator> // for insert_iterator, inserter
+#include <memory>   // for allocator_traits<>::value_type
+
+AtSampleConsensus::AtSampleConsensus() : fRandSamplMode(AtRandomSample::SampleMethod::kUniform) {}
+AtSampleConsensus::~AtSampleConsensus() = default;
+
+/**
+ * @brief Check the goodness of the model
+ *
+ * Evaluates how good the model is in comparison to the points in pointsToCheck.
+ *
+ * @param[in] hitArray AtHits to compare to the model
+ * @param[in/out] model Model to check. Sets Chi2 of the model
+ * @return thenumber of inliers defined by fDistanceThreshold
+ */
+int AtSampleConsensus::evaluateModel(AtTrackModel *model, const std::vector<AtHit> &hitArray)
+{
+   int nbInliers = 0;
+   double weight = 0;
+
+   for (const auto &hit : hitArray) {
+      auto &pos = hit.GetPosition();
+      double error = model->DistanceToModel(pos);
+      error = error * error;
+      if (error < (fDistanceThreshold * fDistanceThreshold)) {
+         nbInliers++;
+         weight += error;
+      }
+   }
+   model->SetChi2(weight / nbInliers);
+   return nbInliers;
+}
+
+std::unique_ptr<AtTrackModel> AtSampleConsensus::GenerateModel(const std::vector<AtHit> &hitArray)
+{
+
+   if (hitArray.size() < fMinModelPoints) {
+      return nullptr;
+   }
+
+   auto model = AtModelFactory::CreateModel(fModelType);
+
+   auto points = AtRandomSample::SamplePoints(model->GetNumPoints(), hitArray, fRandSamplMode);
+   model->ConstructModel(points);
+
+   LOG(debug) << "Testing model" << std::endl;
+   auto nInliers = evaluateModel(model.get(), hitArray);
+   LOG(debug) << "Found " << nInliers << " inliers";
+
+   // If the model is consistent with enough points, save it
+   if (nInliers > fMinModelPoints) {
+      LOG(debug) << "Adding model with nInliers: " << nInliers;
+      return model;
+   }
+
+   return nullptr;
+}
+
+AtPatternEvent AtSampleConsensus::Solve(AtEvent *event)
+{
+   if (event->IsGood())
+      return Solve(event->GetHitArray());
+   return AtPatternEvent();
+}
+
+AtPatternEvent AtSampleConsensus::Solve(const std::vector<AtHit> &hitArray)
+{
+   if (hitArray.size() < fMinModelPoints) {
+      LOG(error) << "Not enough points to solve. Requires" << fMinModelPoints;
+   }
+
+   auto cmp = [](const ModelPtr &a, const ModelPtr &b) { return a->GetChi2() < b->GetChi2(); };
+   auto sortedModels = std::set<ModelPtr, decltype(cmp)>(cmp);
+
+   LOG(debug2) << "Generating " << fIterations << " models";
+   for (int i = 0; i < fIterations; i++) {
+      auto model = GenerateModel(hitArray);
+      if (model != nullptr)
+         sortedModels.insert(std::move(model));
+   }
+   LOG(debug2) << "Created " << sortedModels.size() << " valid models.";
+
+   // Loop through each model, and extract the points that fit each model
+   auto remainHits = hitArray;
+   AtPatternEvent retEvent;
+   for (const auto &model : sortedModels) {
+      if (remainHits.size() < fMinModelPoints)
+         break;
+
+      auto inlierHits = movePointsInModel(model.get(), remainHits);
+      if (inlierHits.size() > fMinModelPoints) {
+         auto track = CreateTrack(model.get(), inlierHits);
+         track.SetTrackID(retEvent.GetTrackCand().size());
+         retEvent.AddTrack(track);
+      }
+   }
+   return retEvent;
+}
+
+AtTrack AtSampleConsensus::CreateTrack(AtTrackModel *model, std::vector<AtHit> &inliers)
+{
+   AtTrack track;
+
+   // Add inliers to our ouput track
+   for (auto &hit : inliers)
+      track.AddHit(std::move(hit));
+
+   model->FitModel(inliers, false);
+
+   track.SetFitPar(model->GetModelPar());
+   track.SetMinimum(model->GetChi2());
+   track.SetNFree(model->GetNFree());
+   return track;
+}
+/**
+ * Moves the entries of hits that are consistent with the model to the returned vector
+ *
+ * @param [in] model
+ * @param[in/out] hits Hits returned are removed from this vector
+ * @return vector containing the AtHits consistent with the model
+ *
+ */
+std::vector<AtHit> AtSampleConsensus::movePointsInModel(AtTrackModel *model, std::vector<AtHit> &hits)
+{
+
+   std::vector<AtHit> retVec;
+   auto itStartEqualRange = hits.end();
+
+   for (auto it = hits.begin(); it != hits.end(); ++it) {
+
+      double error = model->DistanceToModel(it->GetPosition());
+      auto isInModel = (error * error) < (fDistanceThreshold * fDistanceThreshold);
+
+      // Start of sub-vector with hits in model
+      if (isInModel && itStartEqualRange == hits.end()) {
+         itStartEqualRange = it;
+         continue;
+      }
+
+      // End of sub-vector with hits in model.
+      // Move hits in this range to retVec then delete the empty entries
+      if (itStartEqualRange != hits.end() && !isInModel) {
+         retVec.insert(retVec.end(), std::make_move_iterator(itStartEqualRange), std::make_move_iterator(it));
+         hits.erase(itStartEqualRange, it);
+         it = itStartEqualRange;
+         itStartEqualRange = hits.end();
+         continue;
+      }
+   }
+
+   // If the last chunk of the array was in the model, move it and delete empty entries
+   if (itStartEqualRange != hits.end()) {
+      auto it = hits.end();
+      retVec.insert(retVec.end(), std::make_move_iterator(itStartEqualRange), std::make_move_iterator(it));
+      hits.erase(itStartEqualRange, it);
+   }
+   return retVec;
+}
---
+++ AtReconstruction/AtPatternRecognition/AtSampleConsensus.h
@@ -0,0 +1,73 @@
+/*******************************************************************
+// Basic RANSAC Class                                              *
+// Author: J.C. Zamora, jczamorac@gmail.com                        *
+// University of Sao Paulo, 26-08-2020                             *
+********************************************************************/
+
+#ifndef AtSAMPLECONSENSUS_H
+#define AtSAMPLECONSENSUS_H
+
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+
+#include "AtModelFactory.h"
+#include "AtRandomSample.h"
+#include "AtTrack.h" // for AtTrack
+#include "AtTrackModel.h"
+
+#include <Rtypes.h> // for Int_t, Double_t, THashConsistencyHolder, ClassDef
+
+#include <stdio.h> // for size_t
+
+#include <algorithm> // for max
+#include <set>
+#include <utility> // for pair
+#include <vector>  // for vector
+
+class AtEvent;
+class AtPatternEvent;
+
+class AtSampleConsensus {
+protected:
+   AtModelType fModelType{AtModelType::kLINE};
+   AtRandomSample::SampleMethod fRandSamplMode;
+
+   float fIterations{500};       //< Number of interations of sample consensus
+   float fMinModelPoints{30};    //< Required number of points to form a model
+   float fDistanceThreshold{15}; //< Distance a point must be from model to be an inlier
+
+   /**
+    * @brief Min charge for charge weighted fit.
+    *
+    * Minimum charge to include point in charge weighted fit. If set to -1 it
+    * disables charge weighting when fitting the track.
+    */
+   double fChargeThres{-1};
+
+public:
+   AtSampleConsensus();
+   virtual ~AtSampleConsensus();
+
+   AtPatternEvent Solve(AtEvent *event);
+   AtPatternEvent Solve(const std::vector<AtHit> &hitArray);
+
+   void SetRanSamMode(AtRandomSample::SampleMethod mode) { fRandSamplMode = mode; };
+   void SetModelType(AtModelType type) { fModelType = type; }
+   void SetDistanceThreshold(Float_t threshold) { fDistanceThreshold = threshold; };
+   void SetMinHitsLine(Int_t nhits) { fMinModelPoints = nhits; };
+   void SetNumItera(Int_t niterations) { fIterations = niterations; };
+   // void SetChargeThres(double value) { fChargeThres = value; };
+
+protected:
+   using ModelPtr = std::unique_ptr<AtTrackModel>;
+   std::unique_ptr<AtTrackModel> GenerateModel(const std::vector<AtHit> &hitArray);
+
+   virtual int evaluateModel(AtTrackModel *model, const std::vector<AtHit> &hitsToCheck) = 0;
+   std::vector<AtHit> movePointsInModel(AtTrackModel *model, std::vector<AtHit> &indexes);
+   AtTrack CreateTrack(AtTrackModel *model, std::vector<AtHit> &indexes);
+
+   void SaveTrack(AtTrackModel *model, std::vector<AtHit> &indexes, AtPatternEvent *event);
+};
+
+#endif
